91f1d84ff9d52d236fe470007f726bd50428e769
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-01-13 11:40:19 +0100

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -5,5 +5,11 @@ include './inc/images.inc.php';
 ?>
 <?php include './views/header.php'; ?>
 
+<?php foreach ($imageTitles as $src => $title): ?>
+    <a href="image.php?<?php echo http_build_query(['image' => $src]); ?>">
+        <h3><?php echo e($title); ?></h3>
+        <img src=./images/<?php echo rawurldecode($src); ?> alt=" <?php echo e($title); ?>" />
+    </a>
+<?php endforeach; ?>
 
 <?php include './views/footer.php'; ?>
